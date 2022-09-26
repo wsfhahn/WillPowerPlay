fd7cd12a9d2b04877bf07e5f6efb39d956c29e18
BuildTools <unconfigured@null.spigotmc.org>
2022-09-26 14:46:17 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/EncoderFunction.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+//import org.firstinspires.ftc.teamcode.TestFiles.PIDController;
+//import org.firstinspires.ftc.teamcode.TestFiles.PIDControllerHor;
+
+// Vision imports
+
+
+/*
+ * This is NOT an opmode. This file defines all the hardware on the robot
+ * and some common helper functions (stop motors, reset encoders, etc.)
+ */
+public class EncoderFunction {
+
+    public DcMotor motor1;
+    public DcMotor motor2;
+
+    //private DcMotor LeftVertical ;
+    //private DcMotor RightVertical ;
+
+    public void init(HardwareMap ahwMap) {
+
+    }
+
+    public void moveByTileUnit(double speed, DcMotor motor1, DcMotor motor2, int encoderValue) {
+
+        //motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motor1.setPower(speed);
+        motor2.setPower(speed);
+
+        motor1.setTargetPosition(motor1.getCurrentPosition() + encoderValue);
+        motor2.setTargetPosition(motor2.getCurrentPosition() + encoderValue);
+
+        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        for (int i = 0; i < 1000; i++) {
+            int x = 1 + 1;
+        }
+
+    }
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DpadDrive.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import android.text.method.BaseKeyListener;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.Hardware.EncoderFunction;
+
+@TeleOp(name="Dpad Driving", group="LinearOpmode")
+public class DpadDrive extends LinearOpMode {
+
+
+    EncoderFunction robot = new EncoderFunction();
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FrontHorizontal = null;
+    private DcMotor BackHorizontal = null;
+    private DcMotor LeftVertical = null;
+    private DcMotor RightVertical = null;
+
+    private DcMotor LinearSlide = null;
+
+
+    // @Override
+    // public void init(){
+
+    //    robot.init(HardwareMap);
+    // }
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        double motorSpeed = 1.0;
+        boolean movingToTarget = false;
+
+        FrontHorizontal = hardwareMap.get(DcMotor.class, "FrontHorizontal");
+        BackHorizontal = hardwareMap.get(DcMotor.class, "BackHorizontal");
+        LeftVertical = hardwareMap.get(DcMotor.class, "LeftVertical");
+        RightVertical = hardwareMap.get(DcMotor.class, "RightVertical");
+        //fix this
+
+        FrontHorizontal.setDirection(DcMotorSimple.Direction.FORWARD);
+        BackHorizontal.setDirection(DcMotorSimple.Direction.REVERSE);
+        LeftVertical.setDirection(DcMotorSimple.Direction.FORWARD);
+        RightVertical.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.dpad_right) {
+                robot.moveByTileUnit(1, FrontHorizontal, BackHorizontal, 400);
+            }
+
+            else if (gamepad1.dpad_left) {
+                robot.moveByTileUnit(-1, FrontHorizontal, BackHorizontal, 400);
+            }
+
+            else if (gamepad1.dpad_up) {
+                robot.moveByTileUnit(1, LeftVertical, RightVertical, 400);
+            }
+
+            else if (gamepad1.dpad_down) {
+                robot.moveByTileUnit(-1, FrontHorizontal, BackHorizontal, 400);
+            }
+
+            else {
+                FrontHorizontal.setPower(0);
+                BackHorizontal.setPower(0);
+                LeftVertical.setPower(0);
+                RightVertical.setPower(0);
+
+            }
+        }
+    }
+}
+
+
+
